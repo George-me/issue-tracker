66908ea9c6b389062d4cb3e0e796444b9af90600
George Mekhael <georgexp@live.com>
2024-07-16 16:58:39 +0400

    Initial commit

---
+++ app/NavBar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import { IoIosBug } from "react-icons/io";
+
+const NavBar = () => {
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <IoIosBug />
+      </Link>
+      <ul className="flex space-x-6">
+        <li>
+          <Link href="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link href="/issues">Issues</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
